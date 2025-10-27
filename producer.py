22ba574f2fddabed639f26369ea7475bf71dde84
vtt2840 <vuthutrang2840@gmail.com>
2025-10-28 01:08:23 +0700

    crawl data

--- producer.py
+++ producer.py
@@ -2,10 +2,9 @@ import os
 import time
 import json
 import requests
-from datetime import datetime
+from datetime import datetime, timezone
 from kafka import KafkaProducer
 from hdfs import InsecureClient
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -18,13 +17,13 @@ TOPIC = os.getenv("TOPIC")
 HDFS_URL = os.getenv("HDFS_URL")
 HDFS_PATH = os.getenv("HDFS_PATH")
 
-
 # SETUP 
 producer = KafkaProducer(
     bootstrap_servers=[KAFKA_BROKER],
     value_serializer=lambda x: json.dumps(x).encode("utf-8"),
 )
-print("✅ Connected to Kafka successfully!")
+print("Connected to Kafka successfully!")
+
 hdfs_client = InsecureClient(HDFS_URL, user='hadoop')
 
 def fetch_latest_data():
@@ -35,8 +34,8 @@ def fetch_latest_data():
         return None
 
     record = data["data"][0]
-    now_utc = datetime.utcnow()
-    now_local = datetime.now()
+    now_utc = datetime.now(timezone.utc)  # timezone-aware UTC datetime
+    now_local = datetime.now()  # local datetime
 
     doc = {
         "city": CITY,
@@ -53,9 +52,9 @@ def fetch_latest_data():
     }
     return doc
 
-
 def save_to_hdfs(record):
-    date_str = datetime.utcnow().strftime("%Y/%m/%d")
+    now_utc = datetime.now(timezone.utc)
+    date_str = now_utc.strftime("%Y/%m/%d")
     hdfs_dir = os.path.join(HDFS_PATH, date_str)
     hdfs_client.makedirs(hdfs_dir)
 
@@ -63,10 +62,8 @@ def save_to_hdfs(record):
     with hdfs_client.write(file_path, encoding="utf-8") as writer:
         writer.write(json.dumps(record) + "\n")
 
-
 def send_to_kafka(record):
     producer.send(TOPIC, value=record)
-
 
 if __name__ == "__main__":
     while True:
@@ -75,6 +72,7 @@ if __name__ == "__main__":
             if data:
                 send_to_kafka(data)
                 save_to_hdfs(data)
+                print(f"Sent data for {CITY} at {data['timestamp_utc']}")
         except Exception as e:
             print("Error:", e)
-        time.sleep(60) 
+        time.sleep(60)
